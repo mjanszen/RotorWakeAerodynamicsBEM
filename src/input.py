fc533d811ec967da85804796f6bdebf91145c4aa
Jonas MPN <you@example.com>
2023-03-09 15:46:01 +0100

    Stole Michael's functions

--- src/input.py
+++ src/input.py
@@ -17,22 +17,7 @@ v_0 = 10                            # [m] Wind speed
 tsr = [6,8,10]                      # Tip speed ratios  to be calculated
 
 
-def get_twist(r, r_max):
-    """
-        function to get the twist along the blade in radians
-        r: radial position along the blade in [m]
-        r_max: maximum radius of the blade in [m]
-        out: twist in radians 
-    """
-    return np.radians(14*(1 - r/r_max)) 
 
-def get_chord(r, r_max):
-    """
-        function to calculate chord along the span in m
-        r: radial position along the blade in [m]
-        r_max: maximum radius of the blade in [m]
-    """
-    return 3*(1-r/r_max) + 1 
 
 
 airfoil = pd.read_excel("../data/polar.xlsx",skiprows=3)    # read in the airfoil. Columns [alpha, cl, cd cm]
