c4508ee21c7ad9896f5390dba70f0f588b8752c5
mjanszen <mijanszen@gmail.com>
2023-03-09 15:41:38 +0100

    input data

--- src/main.py
+++ src/main.py
@@ -12,6 +12,16 @@ bem.set_constants(rotor_radius=1,
                   air_density=1.225)
 
 if do["a"]:
+    # Parameters
+
+    radius = 50                         # radius of the rotor
+    n_blades = 3                        # number of blades
+    inner_radius = 0.2 * radius         # inner end of the blade section
+    pitch_deg = -2                      # pitch in degrees
+    pitch = np.radians(pitch_deg)       # pitch angle in radian
+    yaw_angles = np.radians([0,15,30])  # yaw angles to be calculated in radians
+    v_0 = 10                            # [m] Wind speed
+    tsr = [6,8,10]                      # Tip speed ratios  to be calculated
     pass
 
 if do["b"]:
